dfea5ec2bd6ab100e438411efd7fcd14667837a9
Go Nakano <g.nakano555@gmail.com>
2019-07-15 16:41:19 +0900

    prepare click and display method for DOM

--- src/Sort.js
+++ src/Sort.js
@@ -2,13 +2,28 @@ class Insertion {
   constructor(array) {
     this.array = array;
   }
-  sort() {
-    console.log("sort!");
+  sort() {}
+
+  click() {
+    const text = document.getElementById("text");
+    text.addEventListener("click", () => this.display("yeeeeeee"));
+  }
+
+  display(str) {
+    const paragraph = document.createElement("p");
+    console.log(paragraph);
+    paragraph.textContent = str;
+    const textArea = document.getElementById("display_container");
+    textArea.appendChild(paragraph);
   }
 
   returnValue(value) {
     return value;
   }
 }
+
+const insert = new Insertion();
+insert.click();
+insert.display();
 
 module.exports = Insertion;
--- test/_sort.js
+++ test/_sort.js
@@ -3,15 +3,22 @@ const { expect } = require("chai");
 
 describe("Sort", () => {
   it("should be a function", () => {
-    expect(Sort).to.be.a("function");
+    expect(Insertion).to.be.a("function");
   });
   it("should have a sort method", () => {
-    expect(Sort.prototype.sort).to.be.a("function");
+    expect(Insertion.prototype.sort).to.be.a("function");
   });
   it("should return sorted array", () => {
     const insert = new Insertion();
     const array = [2, 3, 4, 7, 5, 8, 6, 1, 9, 10];
     const sorted = insert.sort(array);
     expect(sorted).to.be.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+  it("should return correct value after sorted", () => {
+    const insert = new Insertion();
+    const array = [2, 3, 4, 7, 5, 8, 6, 1, 9, 10];
+    const sorted = insert.sort(array);
+    const position = insert.returnValue(8);
+    expect(position).to.be.eql(`your number ${8} is located index ${7}`);
   });
 });
